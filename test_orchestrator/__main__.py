17d5ef5d1c49cba498e5b0d2f018816b2c0b5c49
Nicole Heinimann <nh51siku@studserv.uni-leipzig.de>
2021-12-17 17:38:53 +0100

    reload only in debug mode

--- test_orchestrator/__main__.py
+++ test_orchestrator/__main__.py
@@ -1,19 +1,23 @@
 import uvicorn
 
+import test_orchestrator.settings
 import test_orchestrator.storage.sql.database
 
 
-def start():
+def start(debug):
     "Launched with `poetry run start` at root level"
     uvicorn.run(
         "test_orchestrator.test_orchestrator:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
+        reload=debug,
     )
     return
 
 
 if __name__ == "__main__":
     Session = test_orchestrator.storage.sql.database.init_db()
-    start()
+    if test_orchestrator.settings.debug_env:
+        start(True)
+    else:
+        start(False)
