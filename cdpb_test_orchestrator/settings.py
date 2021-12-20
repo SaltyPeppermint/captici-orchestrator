49d5fc195bea059f99af37280ca510f607de2e2f
Nicole Heinimann <nh51siku@studserv.uni-leipzig.de>
2021-12-20 17:11:45 +0100

    bug in settings squashed

--- cdpb_test_orchestrator/settings.py
+++ cdpb_test_orchestrator/settings.py
@@ -9,20 +9,26 @@ def is_debug() -> bool:
         return False
 
 
-def repos_dir() -> str:
+def mount() -> str:
     mount_env = os.environ.get("MOUNT")
     if mount_env is None:
         mount_env = "/var/nfs"
-
-    return mount_env + "/repos"
+    return mount_env
 
 
 def tars_dir() -> str:
-    mount_env = os.environ.get("MOUNT")
-    if mount_env is None:
-        mount_env = "/var/nfs"
+    tars_dir = os.environ.get("TARS_DIR")
+    if tars_dir is None:
+        tars_dir = mount() + "/tars"
 
-    return mount_env + "/tars"
+    return tars_dir
+
+
+def repos_dir() -> str:
+    repos_dir = os.environ.get("REPOS_DIR")
+    if repos_dir is None:
+        repos_dir = mount() + "/repos"
+    return repos_dir
 
 
 def namespace() -> str:
@@ -47,7 +53,7 @@ def reg_user() -> str:
 
 
 def db_type() -> str:
-    db_type = os.environ.get("db_type")
+    db_type = os.environ.get("DB_TYPE")
     if db_type is None:
         db_type = "sqlite"
     return db_type
@@ -56,5 +62,5 @@ def db_type() -> str:
 def db_location() -> str:
     db_location = os.environ.get("DB_LOCATION")
     if db_location is None:
-        db_location = "/var/nfs"
+        db_location = mount()
     return db_location
