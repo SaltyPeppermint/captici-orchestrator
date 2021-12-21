9c1d8ae1f699f745d66265ad2dd4f02ede66bb5a
Nicole Heinimann <nh51siku@studserv.uni-leipzig.de>
2021-12-21 18:57:06 +0100

    added sadly needed mypy bug warnings

--- cdpb_test_orchestrator/__main__.py
+++ cdpb_test_orchestrator/__main__.py
@@ -1,3 +1,6 @@
+# type: ignore
+# temporarily disabling pydantic mypy checking due to bug
+# https://github.com/samuelcolvin/pydantic/pull/3175#issuecomment-914897604
 from cdpb_test_orchestrator import start
 
 start()
